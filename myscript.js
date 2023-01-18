30b1cb81e3afca401119997acbad3dd229126881
Tanasead <tanasead.tnz@gmail.com>
2023-01-18 13:32:47 +0700

    complete v.3

--- myscript.js
+++ myscript.js
@@ -1,15 +1,16 @@
-var scoreList = document.getElementsByClassName("score");
-    for (var i = 0; i < scoreList.length; i++) {
+let scoreList = document.getElementsByClassName("score");
+    console.log(scoreList);
+    for (let i = 0; i < scoreList.length; i++) {
       scoreList[i].addEventListener("blur", function (e) {
         localStorage.setItem("score", e.target.value);
         console.log(score);
       });
     }
 
-    function nextPage() {
-        var score = localStorage.getItem("score");
+function nextPage() {
+        let score = localStorage.getItem("score");
         document.getElementById("rating").innerHTML = score;
         
         document.getElementById("first-page").style.display = "none";
         document.getElementById("second-page").style.display = "block";
-    }
+}
